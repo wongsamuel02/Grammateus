f19d19a0ac455a2f71dcbe9d0120532932a64035
wongsamuel02 <wongsamuel02@gmail.com>
2024-10-08 19:36:41 -0400

    Integrating ESLint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,10 +2,20 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { 
+    languageOptions: { 
+      globals: { ...globals.browser, ...globals.node } 
+    },
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect"  // Automatically detect the React version
+      }
+    }
+  },
 ];
