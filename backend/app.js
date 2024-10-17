41f819176d86808b0fd10a9ab6c28f81f59475ae
Andy <Andy_lam20@outlook.com>
2024-10-16 21:59:27 -0400

    completed app.js

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const verifyJWT = require('./middleware/verifyJWT');
 const cookieParser = require('cookie-parser')
 require('dotenv').config();
@@ -22,25 +21,24 @@ app.use(express.json({
         }
     }
 }));
-
 app.use(cors());
-
 app.use(cookieParser());
 
 // DB setup
 const connectToMongoDB = require('./database');
-// connectToMongoDB();
+connectToMongoDB();
 
 // routes
 app.use('/', require('./routes/root'))
+app.use('/rester', require('./routes/register'))
 app.use('/auth', require('./routes/auth'))
-app.use('/reigster', require('./routes/register'))
+app.use('/refresh', require('./routes/refresh'))
+app.use('/logout', require('./routes/logout'))
 
 // Restricted Routes
 app.use(verifyJWT)
 
 // Start the server
-const PORT = 8000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
